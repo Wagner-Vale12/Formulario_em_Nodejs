7c369426a97db6f1cf0d602da56ae0bc402f0965
Wagner-Vale12 <wagner6528@gmail.com>
2026-03-17 01:40:46 -0300

    remove conexao no carregamento da funcao vercel

--- api/index.js
+++ api/index.js
@@ -2,9 +2,6 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const { connectToDatabase } = require("../src/database/connect");
 const app = require("../modules/express");
-
-connectToDatabase();
 
 module.exports = app;
